b433ef365556e9a08ae4919f9ce291b593517215
melissa <nadia.mm.mariduena@gmail.com>
2020-08-31 18:18:41 +0200

    animation stting up | readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-##### ANIMATED Menu in react using react-router-dom and GSAP.
+## ANIMATED Menu in react using react-router-dom and GSAP.
+
+### App (in Progress)
+
+![preview](./src/images-readme/anima-dropdown1.jpg)
+
+<br>
+
+![preview](./src/images-readme/anima-dropdown1.jpg)
+
+<br>
+
+<hr>
+
+<!-- [<img src="./src/images-readme/anima-dropdown1.jpg">](https://youtu.be/7M7biQPpOs0) -->
+
+<br>
+<br>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Hamburger from "./Hamburger";
 
 const Header = () => {
+  const [state, setState] = useState({
+    /*
+
+                    **   the initial 
+                    represents the origin or initial state of the page before changes
+                    in this case its false or null, like in the firegram project(before the user 
+                    click on the button)
+
+
+                    **   clicked
+                    represents the hamburger Menu
+
+
+*/
+
+    initial: false,
+    clicked: null,
+    menuName: "Menu",
+  });
+
+  // --------
+  // the function handleMenu will be specific to the BUTTON
+  // so that when we will click the button this function will run
+  // --------
+
+  const handleMenu = () => {
+    if (state.initial === false) {
+      setState({
+        //This is the state when you first open the page
+        // initial there s nothing so it s NULL,
+        initial: null,
+        //  so then we click and it becomes true
+        clicked: true,
+        // and also when you click on it, the word "Menu" will change to "Close"
+        menuName: "Close",
+      });
+      console.log(1);
+    } else if (state.clicked === true) {
+      //  click when open
+      setState({
+        clicked: !state.clicked,
+        menuName: "Menu",
+      });
+      console.log(2);
+    } else if (state.clicked === false) {
+      //  click to close
+      setState({
+        clicked: !state.clicked,
+        menuName: "Close",
+      });
+      console.log(3);
+    }
+  };
+
   return (
     <header>
       <div className="container">
@@ -13,7 +67,7 @@ const Header = () => {
             </div>
             {/* ---------- */}
             <div className="menu">
-              <button>Menu</button>
+              <button onClick={handleMenu}>Menu</button>
             </div>
           </div>
         </div>
